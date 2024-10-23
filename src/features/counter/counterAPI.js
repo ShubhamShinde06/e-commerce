c1f1d093fe14feee161651259d68d9476e9eb6aa
ShubhamShinde06 <officialshubhamorg@gmail.com>
2024-10-23 20:43:15 +0530

    part 1 - setup

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,6 +1,9 @@
 // A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
+  return new Promise((resolve) =>  {
+  const response =  fetch('http://localhost:8000')
+  const data =  response.json()
+  resolve({data})
+}
   );
 }
